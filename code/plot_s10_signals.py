00b603ad3529b35837c27cfaa7836595a2ccced4
Jan Wohland <jan.wohland@hereon.de>
2022-02-01 15:22:51 +0100

    plotting now arranged by GCM and RCM and plot names updated

--- code/plot_s10_signals.py
+++ code/plot_s10_signals.py
@@ -2,29 +2,32 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cf
 import xarray as xr
+from numpy import unique
+
+
+def list_of_models(ds, model_type):
+    index = -2
+    if model_type == "GCM":
+        index = 1
+    return sorted(list(unique([x.split(".")[index] for x in ds.identifier.values])))
+
 
 def plot_array(ds):
     # prepare plotting
+    RCMs, GCMs = list_of_models(ds, "RCM"), list_of_models(ds, "GCM")
     f, axs = plt.subplots(
-        ncols=6,
-        nrows=5,
-        subplot_kw={"projection": ccrs.PlateCarree()},
-        figsize=(10, 6),
+        ncols=len(GCMs),
+        nrows=len(RCMs),
+        subplot_kw={"projection": ccrs.PlateCarree(), "extent": [-15, 50, 35, 70]},
+        figsize=(10, 8),
         dpi=300,
     )
     cbar_ax = f.add_axes([0.2, 0.12, 0.6, 0.01])
     label = "Wind speed change 2080-2100 minus 1985-2005 [m/s]"
 
-    i, j, GCM_old = -1, 0, "No"
     for ident in sorted(ds.identifier.values):
-        GCM, RCM = ident.split(".")[1], ident.split(".")[3]
-        if GCM != GCM_old:  # move one column to the right for each new GCM
-            # turn off axes in plots without data
-            [axs[j_e, i].set_axis_off for j_e in range(j + 1, 5)]
-            i += 1
-            j = 0
-            GCM_old = GCM
-        ax = axs[j, i]
+        GCM, RCM = ident.split(".")[1], ident.split(".")[-2]
+        ax = axs[RCMs.index(RCM), GCMs.index(GCM)]
         # plot values
         ds["sfcWind"].sel(identifier=ident).plot(
             ax=ax,
@@ -46,13 +49,24 @@ def plot_array(ds):
             colors=["none", "white", "none"],
             add_colorbar=False,
         )
-
-        ax.set_title(GCM + ", " + RCM, fontsize=4)
-        j += 1
         ax.add_feature(cf.COASTLINE)
         ax.add_feature(cf.BORDERS)
         ax.set_extent([-15, 50, 35, 70])
-    plt.subplots_adjust(0.05, 0.15, 0.95, 0.99)
+        ax.set_title("")
+    for i, GCM in enumerate(GCMs):  # add GCM name as column headings
+        axs[0, i].set_title(GCM, fontsize=6)
+    for j, RCM in enumerate(RCMs):  # add RCM name as row y axis
+        axs[j, 0].text(
+            -0.1,
+            0.5,
+            RCM,
+            rotation="vertical",
+            fontsize=5,
+            horizontalalignment="center",
+            verticalalignment="center",
+            transform=axs[j, 0].transAxes,
+        )
+    plt.subplots_adjust(0.05, 0.15, 0.95, 0.99, hspace=0.05, wspace=0.05)
 
 
 def plot_array_CMIP5(
@@ -100,15 +114,16 @@ def plot_array_CMIP5(
         ax.add_feature(cf.BORDERS)
     plt.subplots_adjust(0.05, 0.15, 0.95, 0.99)
 
+
 # CORDEX
 diff = xr.open_dataset("../output/cordex_diff_rcp45.nc")
 plot_array(diff)
 plt.savefig(
-    "../plots/RCM-GCM_windchange.png", dpi=300, facecolor="w", transparent=False
+    "../plots/cordex_windchange_rcp45.png", dpi=300, facecolor="w", transparent=False
 )
 
 # CMIP5
 
 diff = xr.open_dataset("../output/cmip5_diff_rcp45.nc")
 plot_array_CMIP5(diff)
-plt.savefig("../plots/CMIP5_windchange.png", dpi=300, facecolor="w", transparent=False)
+plt.savefig("../plots/cmip5_windchange_rcp45.png", dpi=300, facecolor="w", transparent=False)
