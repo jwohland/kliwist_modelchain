0db3ec2ceafd263b84e446ad2692b33475ded32d
Jan Wohland <jan.wohland@hereon.de>
2022-05-13 11:41:21 +0200

    Added first version of convergence maps

--- code/analyze_LUCAS_profiles.py
+++ code/analyze_LUCAS_profiles.py
@@ -53,7 +53,7 @@ def average_over_region(ds, r_lat_center, r_lon_center):
 
 
 ds_list = [open_and_preprocess(x) for x in experiment_dictionary.keys()]
-ds_all = xr.concat(ds_list, dim="experiment")
+ds_all = xr.concat(ds_list, dim="experiment").compute()
 
 """
 # plot vertical wind profile at example locations
@@ -100,15 +100,23 @@ for lat_offset in [-10, 0, 10]:
 """
 # plot mean change maps at different height levels
 """
+
+ds_grass_minus_eval = ds_all.sel({"experiment": "grass"}) - ds_all.sel(
+    {"experiment": "eval"}
+)
+ds_eval_minus_forest = ds_all.sel({"experiment": "eval"}) - ds_all.sel(
+    {"experiment": "forest"}
+)
+
 rotated_pole = ccrs.RotatedPole(-162, 39.25)
 # pole location can be obtained from ds.rotated_pole.attrs["grid_north_pole_longitude"], ds.rotated_pole.attrs["grid_north_pole_latitude"]
 f, axs = plt.subplots(
     ncols=2, nrows=5, figsize=((10, 14)), subplot_kw={"projection": rotated_pole}
 )
 
+
 for j, lev in enumerate(list(ds_all.lev.values)[-5:]):
-    diff1 = ds_all.sel({"experiment": "grass"}) - ds_all.sel({"experiment": "eval"})
-    diff1["S"].sel({"lev":lev}).plot(
+    ds_grass_minus_eval["S"].sel({"lev": lev}).plot(
         ax=axs[j, 0],
         # x="lon",
         # y="lat",
@@ -117,8 +125,7 @@ for j, lev in enumerate(list(ds_all.lev.values)[-5:]):
         extend="both",
         cbar_kwargs={"label": "Wind speed change [m/s]", "orientation": "horizontal"},
     )
-    diff2 = ds_all.sel({"experiment": "eval"}) - ds_all.sel({"experiment": "forest"})
-    diff2["S"].sel({"lev":lev}).plot(
+    ds_eval_minus_forest["S"].sel({"lev": lev}).plot(
         ax=axs[j, 1],
         # x="lon",
         # y="lat",
@@ -145,3 +152,78 @@ axs[0, 1].set_title("FOREST - GRASS")
 
 plt.tight_layout()
 plt.savefig("../plots/LUCAS/REMO_absolute_change_winds_vertical.png", **FIG_PARAMS)
+
+"""
+# plot convergence maps for different absolute and relative convergence criteria
+"""
+
+# absolute thresholds
+# per location check
+# a) lowest level wind change is larger than threshold
+# b) lowest level at which wind change smaller than threshold
+threshold = 0.1  # m/s
+convergence_map = np.zeros((121, 129))
+
+#  todo horribly slow as expected from looping
+for i_rlat in range(121):
+    for i_rlon in range(129):
+        print(i_rlon)
+        tmp = (
+            ds_eval_minus_forest.isel({"rlat": i_rlat, "rlon": i_rlon})["S"].values
+            < threshold
+        )
+        if tmp[0] == False:  # criterion a
+            if (tmp == True).any():
+                convergence_level = np.where(tmp == True)[0][
+                    -1
+                ]  # criterion b. Note that levels are sorted from top to bottom
+                convergence_geopotential = float(
+                    ds_all["FI"]
+                    .sel({"experiment": "grass"})
+                    .isel({"rlat": i_rlat, "rlon": i_rlon, "lev": convergence_level})
+                    .values
+                )
+                convergence_map[i_rlat, i_rlon] = convergence_geopotential
+    break
+
+# todo somewhat better approach but needs double checking whether correct
+rotated_pole = ccrs.RotatedPole(-162, 39.25)
+# pole location can be obtained from ds.rotated_pole.attrs["grid_north_pole_longitude"], ds.rotated_pole.attrs["grid_north_pole_latitude"]
+f, axs = plt.subplots(
+    ncols=2, nrows=5, figsize=((6, 14)), subplot_kw={"projection": rotated_pole}
+)
+
+for i, threshold in enumerate([.4, 0.2, 0.1, 0.05]):
+    for j, ds in enumerate([ds_eval_minus_forest, ds_grass_minus_eval]):
+        tmp = ds["S"] < threshold  # binary classifier
+        tmp = tmp.cumsum(dim="lev").argmax(
+            dim="lev"
+        )  # levels are sorted from top of atmosphere to ground level. This locates the lowermost level where change is smaller than threshold
+        ds["convergence_map"] = tmp
+        ds["convergence_map"].plot(
+            levels=[20, 21, 22, 23, 24],
+            cbar_kwargs={"label": "Convergence level"},
+            ax=axs[i, j],
+            extend="min",
+        )
+        add_coast_boarders(axs[i, j])
+        axs[i, j].set_title("")
+
+    axs[i, 0].text(
+        -0.1,
+        0.5,
+        "threshold =" + str(threshold),
+        rotation="vertical",
+        fontsize=10,
+        transform=axs[i, 0].transAxes,
+        **TEXT_PARAMS
+    )
+
+axs[0, 0].set_title("EVAL - FOREST")
+axs[1, 0].set_title("GRASS - EVAL")
+
+plt.savefig(
+    "../plots/LUCAS/REMO_convergence_maps.png",
+    dpi=300,
+    facecolor="white",
+)
