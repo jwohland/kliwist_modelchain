82e6ec99de31ac9243a578efd884b780b40021e3
Jan Wohland <jan.wohland@hereon.de>
2022-05-31 08:57:08 +0200

    typo

--- code/run_all.py
+++ code/run_all.py
@@ -22,7 +22,7 @@ Plots
 plot_s10_maps.make_s10_maps()
 # Scatter plots of wind change per country
 plot_s10_scatter.make_s10_scatter(onshore=True)
-plot_s10_scatter.make_s10_scatter(onshore=True)
+plot_s10_scatter.make_s10_scatter(onshore=False)
 # Heatmaps of wind per country
 plot_s10_country_heatmaps.make_s10_heatmaps(onshore=True)
 plot_s10_country_heatmaps.make_s10_heatmaps(onshore=False)
